7e0fcaa9ee95be317a2acab8060b2e939731bf95
unknown <you@example.com>
2021-02-09 11:00:30 +0700

    first commit

---
+++ lib/app_screens/Home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.orangeAccent,
+      child: Text("flight"),
+    );
+  }
+}
